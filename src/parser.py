ed4bc8250e2bb40bc892bed592d3c4a24da4d5ba
asdidkf <fernan.gautrin@hotmail.com>
2026-06-03 19:17:44 -0700

    feat: agregar plantillas de las funciones que se requieren.

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,16 @@
-def read(tokens):
-    pass
+import main
+
+def read(terminal, estado):
+    if estado:
+        if main.tokens[0][0] == terminal:
+            main.tokens.pop(0)
+            return True
+        else: return False
+    else:
+        if main.tokens[0][1] == terminal:
+            main.tokens.pop(0)
+            return True
+        else: return False
 
 def program():
     if not item_list(): raise Exception
@@ -12,4 +23,65 @@ def item_list():
     else: return True
 
 def item():
+    if decl(): return True
+    elif stmt(): return True
+    else: return False
+
+def decl():
+    if read("var", True):
+        if type():
+            if read("id", False):
+                if read(";", True):
+                    return True
+    return False 
+
+def type():
+    pass
+
+def stmt():
+    pass
+
+def block():
+    pass
+
+def assign():
+    pass
+
+def print():
+    pass
+
+def if_stmt():
+    pass
+
+def else_part():
+    pass
+
+def while_stmt():
+    pass
+
+def expr():
+    if not logic_or(): raise Exception
+
+def logic_or():
+    pass
+
+def logic_and():
+    pass
+
+def equality():
+    pass
+
+def relation():
+    pass
+
+def term():
+    pass
+
+def factor():
+    pass
+
+def unary():
+    pass
+
+def primary():
     pass

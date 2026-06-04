9ff66c495d4418a419ed41c685173a20b847d965
asdidkf <fernan.gautrin@hotmail.com>
2026-06-03 23:34:51 -0700

    feat: implementar nodos para generar AST

---
+++ src/ast.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, category, value):
+        self.children = []
+        self.category = category
+        self.value = value 
+    
+    def insert(category, value):
+        self.children.append(Node(category, value))
+
+

b3621f2209fe0e919103f699e41e75094d959a06
asdidkf <fernan.gautrin@hotmail.com>
2026-06-04 01:04:43 -0700

    feat: modificar clase Node

--- src/ast.py
+++ src/ast.py
@@ -1,12 +1,14 @@
 class Node:
-    def __init__(self, category, value, parent, line):
-        self.children = []
+    def __init__(self, category, value=None, line=None):
         self.category = category
         self.value = value
         self.line = line
-    
-    def add_child(self, category, value, line):
-        self.children.append(Node(self.category, self.value, self.line))
+        self.children = []
 
-    def print_tree(self):
-        pass
+    def add_child(self, node):
+        self.children.append(node)
+
+    def print_tree(self, level=0):
+        print("  " * level + f"{self.category}: {self.value}")
+        for child in self.children:
+            child.print_tree(level + 1)

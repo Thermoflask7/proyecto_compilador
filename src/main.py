bd0bd263f0efa5ae02417466c6f25d873bce6d3d
asdidkf <fernan.gautrin@hotmail.com>
2026-06-12 16:46:48 -0700

    fix: arreglar import ciclico en main y parser.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 import lexer
 import parser
+import ast
+
 file_path = "file.txt"
 
 parser.tokens = lexer.analisis_lexico(file_path)
 #tokens -> [string, tokentype, [line, column]]
-print(parser.program())
+tree = parser.program()
+tree.print_tree()
